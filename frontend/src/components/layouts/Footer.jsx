74c0f926bc85c1feef38e44507714909d2f49796
Angela Barrenechea <angelacba86@gmail.com>
2025-09-02 23:03:39 -0500

    create footer

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,50 @@
+import "./Footer.css";
+import {
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillLinkedin,
+} from "react-icons/ai";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="inner footer-container">
+        <div className="links">
+          <nav>
+            <ul>
+              <a href="#">
+                <li>Sobre Nosotros</li>
+              </a>
+              <a href="#">
+                <li>Contáctanos</li>
+              </a>
+            </ul>
+          </nav>
+          <div className="social-media">
+            <a href="#">
+              <span className="red">
+                <AiFillInstagram />
+              </span>
+            </a>
+            <a href="#">
+              <span className="red">
+                <AiFillFacebook />
+              </span>
+            </a>
+            <a href="#">
+              <span className="red">
+                <AiFillLinkedin />
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="copyright">
+          <small>
+            © 2025 StreamMap - Todos los contenidos externos son propiedad de
+            sus legítimos propietarios.
+          </small>
+        </div>
+      </div>
+    </footer>
+  );
+};
