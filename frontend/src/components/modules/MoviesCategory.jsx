9cd3735168072fe6847984dc96b646817c6e1713
Angela Barrenechea <angelacba86@gmail.com>
2025-09-02 23:05:31 -0500

    create moviesCategory

---
+++ frontend/src/components/modules/MoviesCategory.jsx
@@ -0,0 +1,58 @@
+import "./MoviesCategory.css";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+
+export const MoviesCategory = ({ type, category, title }) => {
+  // const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8];
+  const containerRef = useRef(null);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/${type}/${category}`);
+        setMovies(response.data);
+      } catch (error) {
+        console.error("Error al cargar películas:", error.message);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+  const handleNext = () => {
+    containerRef.current.scrollBy({ left: 220, behavior: "smooth" });
+  };
+
+  const handlePrev = () => {
+    containerRef.current.scrollBy({ left: -220, behavior: "smooth" });
+  };
+  return (
+    <div className="content-movie-category">
+      <h2>{title}</h2>
+      <div className="movies-container">
+        <button onClick={handlePrev}>
+          <GrFormPrevious />
+        </button>
+        <div className="movies-subcontainer" ref={containerRef}>
+          {movies.map((movie, index) => (
+            <div className="movie" key={movie.id}>
+              <img
+                src={
+                  movie.poster
+                    ? `https://image.tmdb.org/t/p/w200${movie.poster}`
+                    : "https://placehold.co/200x350?text=Sin+Imagen"
+                }
+                alt={movie.title}
+              />
+              <span>{index+1}</span>
+            </div>
+          ))}
+        </div>
+        <button onClick={handleNext}>
+          <GrFormNext />
+        </button>
+      </div>
+    </div>
+  );
+};
