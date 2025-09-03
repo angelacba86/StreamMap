45b3d68afb42504ca3a58dc5991bab5444efcdd8
Angela Barrenechea <angelacba86@gmail.com>
2025-09-02 23:06:40 -0500

    insert components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
-function Home(){
-    return(
-<></>
-    )
+import { Banner, StepsCards, MoviesCategory } from "../components";
+function Home() {
+  return (
+    <>
+      <Banner />
+      <StepsCards />
+      <MoviesCategory
+        type="movie"
+        category="popular"
+        title="🎬 Películas Populares"
+      />
+      <MoviesCategory
+        type="tv"
+        category="popular"
+        title="📺 Series Populares"
+      />
+    </>
+  );
 }
-export default Home
+export default Home;
