fde4b34f90bc732125b4a3726aa0aabfc7bf0efa
Angela Barrenechea <angelacba86@gmail.com>
2025-09-03 16:49:52 -0500

    add resultFull route

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -5,10 +5,12 @@ const {
   searchResultsCtrl,
   resultDetailCtrl,
   resultProvidersCtrl,
+  resultFullCtrl,
 } = require("../controllers/searchCtrl");
 
 router.get("/", searchResultsCtrl);
 router.get("/:type/:id", resultDetailCtrl);
-router.get("/:type/:id/watch/providers",resultProvidersCtrl)
+router.get("/:type/:id/watch/providers", resultProvidersCtrl);
+router.get("/:type/:id/full", resultFullCtrl);
 
 module.exports = router;
