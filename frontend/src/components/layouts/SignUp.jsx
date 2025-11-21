f99527569aa82d4b191dd3106f2a9befe5f3e2e6
Angela Barrenechea <angelacba86@gmail.com>
2025-11-21 14:10:05 -0500

    create - new signup working

---
+++ frontend/src/components/layouts/SignUp.jsx
@@ -0,0 +1,43 @@
+import "./SignUp.css";
+import { Input, UiButton } from "../index";
+
+export const SignUp = ({ openLogin }) => {
+  return (
+    <>
+      <form className="signup">
+        <label htmlFor="name">Nombre(s)</label>
+        <Input type="text" placeholder="Coloca tu nombre aquí" id="name" />
+        <label htmlFor="lastname">Apellido(s)</label>
+        <Input
+          type="text"
+          placeholder="Coloca tu Apellido aquí"
+          id="lastname"
+        />
+        <label htmlFor="email">Email</label>
+        <Input type="email" placeholder="Coloca tu email aquí" id="email" />
+        <label htmlFor="password">Contraseña</label>
+        <Input
+          type="password"
+          placeholder="Coloca tu contraseña aquí"
+          id="password"
+        />
+        <label htmlFor="password2">Repite la Contraseña</label>
+        <Input
+          type="password"
+          placeholder="Repite tu contraseña aqui"
+          id="password2"
+        />
+        <div className="button-content">
+          <UiButton>Enviar</UiButton>
+        </div>
+        <hr />
+        <small>
+          ¿Ya tienes una cuenta?{" "}
+          <button type="button" className="link-btn" onClick={openLogin}>
+            Ingresa aqui
+          </button>
+        </small>
+      </form>
+    </>
+  );
+};
